0d5681b1a1f2bf82480a334de4a16018b97482a3
Drollpig51 <2patryk0@wp.pl>
2024-09-29 18:52:29 +0200

    TaskService added,

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../tasks/task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -9,12 +11,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  @Input({required: true}) userId!: string;
   @Output() closed = new EventEmitter<void>();
+  private tasksService = inject(TasksService);
+
   enteredTitle: string = '';
   enteredSummary: string = '';
   enteredDate: string = '';
 
   onCancel(){
+    this.closed.emit();
+  }
+
+  onSubmit(){
+    this.tasksService.addTask({title: this.enteredTitle, summary: this.enteredSummary, dueDate: this.enteredDate}, this.userId);
     this.closed.emit();
   }
 }
